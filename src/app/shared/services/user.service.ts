e6cc101c4b3b0313ff786ae07374f55dd75c9871
SergioC2K <SergioC2K>
2022-12-10 09:18:21 -0500

    modifica end point

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { ICreateUserResponse, IUserResponse } from "../models";
 import { HttpClient } from "@angular/common/http";
+import { environment } from "../../../environments/environment";
 
 @Injectable({
   providedIn: "root"
 })
 export class UserService {
 
-  private apiUrl: string = `tucajaexpress-api/user`;
+  private apiUrl: string = `${environment.tuCajaExpressApi}/user`;
+  // private apiUrl: string = `tucajaexpress-api/user`;
 
   constructor(private http: HttpClient) {
   }
