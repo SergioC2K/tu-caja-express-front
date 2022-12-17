431af718a305bf86a32f0cc4d94a4ba02596fd1a
SergioC2K <SergioC2K>
2022-12-17 14:53:40 -0500

    Ajusta sección selección tipo de cajas

--- src/app/admin/packages/zone-and-prices/zone-and-prices.component.ts
+++ src/app/admin/packages/zone-and-prices/zone-and-prices.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-zone-and-prices',
-  templateUrl: './zone-and-prices.component.html',
-  styleUrls: ['./zone-and-prices.component.css']
+  selector: "app-zone-and-prices",
+  templateUrl: "./zone-and-prices.component.html",
+  styleUrls: ["./zone-and-prices.component.css"]
 })
 export class ZoneAndPricesComponent implements OnInit {
+  list: any[] = [
+    {
+      name: 'Photos',
+    },
+    {
+      name: 'Recipes',
+    },
+    {
+      name: 'Work',
+    },
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  public onSubmit(): void {
+
+  }
+
+  public deletePackage(): void {
+
+  }
 }
