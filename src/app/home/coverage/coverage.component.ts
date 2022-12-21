6130b2e0b37e78da268535fac1dc88bde34a3b36
SergioC2K <SergioC2K>
2022-12-20 19:39:09 -0500

    Modifica boton whatsapp

--- src/app/home/coverage/coverage.component.ts
+++ src/app/home/coverage/coverage.component.ts
@@ -12,4 +12,27 @@ export class CoverageComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  url: any;
+  phone: string = "3216339624";
+  msg: string = "Hola tu caja, me entere que puedes ser mi aliado!"
+
+  public isMobile() {
+    return (
+      (navigator.userAgent.match(/Android/i)) ||
+      (navigator.userAgent.match(/webOS/i)) ||
+      (navigator.userAgent.match(/iPhone/i)) ||
+      (navigator.userAgent.match(/iPod/i)) ||
+      (navigator.userAgent.match(/iPad/i)) ||
+      (navigator.userAgent.match(/BlackBerry/i))
+    );
+  }
+  openWhatsAppLink() {
+    var opcion = "web";
+    if (this.isMobile()) {
+      opcion = "api";
+    }
+    this.url = "https://" + opcion + ".whatsapp.com/send?phone=57" + this.phone + "&text=" + this.msg;
+    window.open(this.url, '_blank');
+  }
 }
